83272f73d8c7d2a2d4938d467c16694e65c2de63
kimakima <g.kimakima@gmail.com>
2015-05-28 19:58:30 +0900

    remove comment lines

--- rakuten_blog_retrieve.py
+++ rakuten_blog_retrieve.py
@@ -23,29 +23,20 @@ opener = urllib2.build_opener()
 soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(url_origin).read())
 
 # title
-#for st_title in soup.find("h2").find("a"):
-#	print st_title
 print str(soup.find("h2").find("a").string)
 
 # datetime
-#print str(soup.find("div",align="right")).count("\n")
 print str(soup.find("div",align="right")).rsplit("\n")[2].strip()
 	
 # content & strip html tags
 for st_contents in soup.findAll(attrs={"class":"dText break-word"}):
-#	print st_contents.name
-#	print st_contents.string
-#	print st_contents.attrs
-#	print re.sub(r'<.*>',"",str(st_contents))
 	print re.sub(r'<br />',"",str(st_contents))
-#	print str(st_contents)
 
 for st_link_image in soup.find(attrs={"class":"dText break-word"}).findAll("img",src=True):
 	print "link to image\t:" + st_link_image['src']
 
 # link to past content
 for st_past_link in soup.findAll(attrs={"class":"main_title"}):
-#	print type(st_past_link)
 	if "過去" in str(st_past_link):
 		st_past_link = url_base + st_past_link.find("a",href=True)['href']
 print "link to past content\t:" + st_past_link
