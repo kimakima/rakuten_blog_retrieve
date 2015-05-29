677a70efd8f17132ad90570adbebd2f82f8e96ff
kimakima <g.kimakima@gmail.com>
2015-05-29 20:00:40 +0900

    add retrieve loop

--- rakuten_blog_retrieve.py
+++ rakuten_blog_retrieve.py
@@ -4,23 +4,21 @@ import BeautifulSoup
 import urllib2
 import re
 
-url_base = "http://plaza.rakuten.co.jp"
+url_base = "http://plaza.rakuten.co.jp/"
 
-ini_file = open('rakuten_blog_retrieve.ini','r')
+ini_file = open('./rakuten_blog_retrieve.ini','r')
 for line in ini_file:
 	url_blog = line
 ini_file.close()
+print url_blog
 
 argvs = sys.argv
 if len(argvs) == 2:
-	url_origin = url_base + argvs[1]
+	st_past_link = url_base + argvs[1]
 else:
-	url_origin = url_base + url_blog
+	st_past_link = url_base  + url_blog
 	
-print url_origin
-
-opener = urllib2.build_opener()
-soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(url_origin).read())
+print st_past_link
 
 # title
 def get_title(soup):
@@ -37,7 +35,7 @@ def get_content(soup):
 
 # link to past content
 def get_past_link(soup):
-	return = str(soup.find("li",{"class":"sideNext"}).find("a",href=True)['href'])
+	return str(soup.find("li",{"class":"sideNext"}).find("a",href=True)['href'])
 
 # link to images
 def get_link_image(soup):
@@ -47,9 +45,21 @@ def get_link_image(soup):
 		print st_link_image['src']
 	return image_links
 
-print "title\t:" + get_title(soup)
-print "datetime\t:" + get_datetime(soup)
-print "content\t:\n" + get_content(soup)
-for i in get_link_image(soup):
-	print "link to images\t:" + i
-print "link to past content\t:" + get_past_link(soup)
+opener = urllib2.build_opener()
+loop_flg = True
+while loop_flg == True:
+	soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(st_past_link).read())
+
+	print "title\t:" + get_title(soup)
+	print "datetime\t:" + get_datetime(soup)
+	print "content\t:\n" + get_content(soup)
+	for i in get_link_image(soup):
+		print "link to images\t:" + i
+	st_past_link = get_past_link(soup)
+
+	if(len(st_past_link) == 0):
+		loop_flg = False
+	else:
+		st_past_link = url_base + re.sub("^/","",st_past_link)
+
+	print "link to past content\t:" + st_past_link
