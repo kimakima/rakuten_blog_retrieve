141a340790425321528c213f5903c4c91a7d70cf
kimakima <g.kimakima@gmail.com>
2015-06-03 20:05:11 +0900

    add file: rakuten_blog_list.py

---
+++ rakuten_blog_list.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+import sys
+from bs4 import BeautifulSoup
+import urllib2
+import re
+
+#BASE_URL = "http://plaza.rakuten.co.jp/"
+#IMAGE_URL = "http://image.space.rakuten.co.jp"
+
+RAKUTEN_BLOG_NAME = "kimakimach"
+DAIRYALL_URL = "http://plaza.rakuten.co.jp/" + RAKUTEN_BLOG_NAME + "/diaryall/"
+
+st_link = DAIRYALL_URL
+
+soup = BeautifulSoup(urllib2.urlopen(st_link).read())
+
+for st_block in soup.find_all('li',class_="loListItem"):
+	for st_title in st_block.find_all('h2',class_="diary_title"):
+		print st_title.string
+	for st_link in st_block.find_all('a'):
+		print st_link.get('href')
+#	st_title = st_link.find_all('h2',lass_="diary_title")
+
+#for st_title in soup.find_all('h2',class_="loListTtl diary_title"):
+#	print st_title
