fc8185ce44a58adb4212f6e14b14ac00885b6cd9
kimakima <g.kimakima@gmail.com>
2015-06-05 18:47:53 +0900

    ページャーリンクの重複削除

--- rakuten_blog_list.py
+++ rakuten_blog_list.py
@@ -20,9 +20,11 @@ for st_block in soup.find_all('li',class_="loListItem"):
 	for st_link in st_block.find_all('a'):
 		print st_link.get('href')
 
+li_pagers = []
 for st_pager_area in soup.find_all('p',class_="pagerArea"):
 	for st_pager in st_pager_area.find_all('a'):
-		print st_pager.get('href')
+		li_pagers.append(str(st_pager.get('href')))
+print list(set(li_pagers))
 
 #	st_title = st_link.find_all('h2',lass_="diary_title")
 
