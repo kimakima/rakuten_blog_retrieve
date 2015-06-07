d5d2fc0c4f457f9913172cee29afb48d453e8b2e
kimakima <g.kimakima@gmail.com>
2015-06-07 15:15:20 +0900

    関数名変更、pop_pager_link()追加など

--- rakuten_blog_list.py
+++ rakuten_blog_list.py
@@ -12,6 +12,7 @@ RAKUTEN_BLOG_NAME = "kimakimach"
 class BlogList:
 	def __init__(self,RAKUTEN_BLOG_NAME):
 		self.st_link = "http://plaza.rakuten.co.jp/" + RAKUTEN_BLOG_NAME + "/diaryall/"
+		self.li_pager_link = []
 		
 	def get_pager_link(self):
 		soup = BeautifulSoup(urllib2.urlopen(self.st_link).read())
@@ -20,12 +21,21 @@ class BlogList:
 		for st_pager_area in soup.find_all('p',class_="pagerArea"):
 			for st_pager in st_pager_area.find_all('a'):
 				li_pagers.append(str(st_pager.get('href')))
-		li_pagers = list(set(li_pagers))
+		self.li_pager_link = list(set(li_pagers))
 		# ベースのURLからPagerのListを取得してから、Listの先頭にベースのURLを追加することで、完全なPagerのListになる。
-		li_pagers.insert(0, self.st_link)
-		return li_pagers
+		self.li_pager_link.insert(0, self.st_link)
+		if len(self.li_pager_link):
+			return True
+		else:
+			return False
 
-	def get_titles(self,st_poped_link):
+	def pop_pager_link(self):
+		if len(self.li_pager_link):
+			return self.li_pager_link.pop(0)
+		else:
+			return False
+
+	def get_contents_data(self,st_poped_link):
 		soup = BeautifulSoup(urllib2.urlopen(st_poped_link).read())
 
 		li_contents = []
@@ -39,10 +49,12 @@ class BlogList:
 		return li_contents
 
 blog_list = BlogList(RAKUTEN_BLOG_NAME)
-li_pager_link = blog_list.get_pager_link()
+print blog_list.get_pager_link()
 
-while len(li_pager_link):
-	li_contents = blog_list.get_titles(li_pager_link.pop(0))
+st_link = blog_list.pop_pager_link()
+while st_link:
+	li_contents = blog_list.get_contents_data(st_link)
 
 	for i in li_contents:
 		print i[0].encode('utf-8') + "\t" + i[1].encode('utf-8')
+	st_link = blog_list.pop_pager_link()
