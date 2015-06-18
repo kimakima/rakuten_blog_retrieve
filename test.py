3e0a188fa6b266c5b5f0cf6deeec44082eca0905
kimakima <g.kimakima@gmail.com>
2015-06-18 13:35:38 +0900

    不要なファイルの削除

--- test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-st_date = "April 17, 2008 11:14:18 PM"
-
-dt_date = datetime.strptime(st_date, "%B %d, %Y %I:%M:%S %p")
-print dt_date.strftime("%Y-%m-%d %H:%m:%S")
-
-
