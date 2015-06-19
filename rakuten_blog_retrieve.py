da250435997f3cb1a8b57379bb26a9955a6a88e7
kimakima <g.kimakima@gmail.com>
2015-06-19 19:44:12 +0900

    各関数をクラス BlogBody にまとめた。

--- rakuten_blog_retrieve.py
+++ rakuten_blog_retrieve.py
@@ -14,57 +14,66 @@ if len(argvs) == 2:
 else:
 	sys.exit()
 
-target_url = []
-fh_link = open(st_link_filename,'r')
-for line in fh_link:
-	target_url.append(line.rstrip().split('\t')[1])
-fh_link.close()
-print str(target_url)
+class BlogBody:
+	def __init__(self):
+		self.target_url = []
+		opener = urllib2.build_opener()
+
+	def read_link_file(self, st_link_filename):
+		fh_link = open(st_link_filename,'r')
+		for line in fh_link:
+			self.target_url.append(line.rstrip().split('\t')[1])
+		fh_link.close()
+		print str(self.target_url)
+
+	def retrieve_link_url(self):
+		self.soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(self.target_url.pop(0)).read())
 	
-# title
-def get_title(soup):
-	return str(soup.find("h2").find("a").string)
+	# title
+	def get_title(self):
+		return str(self.soup.find("h2").find("a").string)
 
-# datetime
-def get_datetime(soup):
-	st_date = str(soup.find("div",align="right")).rsplit("\n")[2].strip()
-	dt_date = datetime.strptime(st_date, "%B %d, %Y %I:%M:%S %p")
-	return dt_date.strftime("%Y-%m-%d %H:%m:%S")
+	# datetime
+	def get_datetime(self):
+		st_date = str(self.soup.find("div",align="right")).rsplit("\n")[2].strip()
+		dt_date = datetime.strptime(st_date, "%B %d, %Y %I:%M:%S %p")
+		return dt_date.strftime("%Y-%m-%d %H:%m:%S")
 
-# content & strip html tags
-def get_content(soup):
-	for st_contents in soup.findAll(attrs={"class":"dText break-word"}):
-		st_contents = str(st_contents)
-		st_contents = re.sub(r'<div class=\"dText break-word\">',"",st_contents)
-		st_contents = re.sub(r'</div>',"",st_contents)
-		st_contents = re.sub(r'\r\n','\n',st_contents)
-		return re.sub(r'<br />',"",st_contents)
+	# content & strip html tags
+	def get_content(self):
+		for st_contents in self.soup.findAll(attrs={"class":"dText break-word"}):
+			st_contents = str(st_contents)
+			st_contents = re.sub(r'<div class=\"dText break-word\">',"",st_contents)
+			st_contents = re.sub(r'</div>',"",st_contents)
+			st_contents = re.sub(r'\r\n','\n',st_contents)
+			return re.sub(r'<br />',"",st_contents)
 
-# link to past content
-def get_past_link(soup):
-	return str(soup.find("li",{"class":"sideNext"}).find("a",href=True)['href'])
+	# link to past content
+	def get_past_link(self):
+		return str(self.soup.find("li",{"class":"sideNext"}).find("a",href=True)['href'])
 
-# link to images
-def get_link_image(soup):
-	image_links=[]
-	for st_link_image in soup.find(attrs={"class":"dText break-word"}).findAll("img",src=True):
-		if IMAGE_URL in st_link_image['src']:
-			image_links.append(st_link_image['src'])
+	# link to images
+	def get_link_image(self):
+		image_links=[]
+		for st_link_image in self.soup.find(attrs={"class":"dText break-word"}).findAll("img",src=True):
+			if IMAGE_URL in st_link_image['src']:
+				image_links.append(st_link_image['src'])
 #		print st_link_image['src']
-	return image_links
+		return image_links
 
-opener = urllib2.build_opener()
+blog_body = BlogBody()
+blog_body.read_link_file(st_link_filename)
+
 while True:
 	try:
-		soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(target_url.pop(0)).read())
-
 		print "----------"
-		print "title\t:" + get_title(soup)
-		print "datetime\t:" + get_datetime(soup)
-		print "content\t:\n" + get_content(soup)
-		for i in get_link_image(soup):
+		blog_body.retrieve_link_url()
+		print "title\t:" + blog_body.get_title()
+		print "datetime\t:" + blog_body.get_datetime()
+		print "content\t:\n" + blog_body.get_content()
+		for i in blog_body.get_link_image():
 			print "link to images\t:" + i
-		st_link = get_past_link(soup)
+		st_link = blog_body.get_past_link()
 
 #		print "link to past content\t:" + st_link
 	except:
